4a10d341a67e9ac0c8cb70e72a12183cc4c5365d
Sanjeet Sidhu <sanjeetu3@gmail.com>
2021-04-08 22:37:56 -0700

    trying to get netlify to work, no insta components this time

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 import usePosts from '../hooks/usePosts';
 import PostPreview from '../components/PostPreview';
 import Hero from '../components/Hero';
-import Insta from '../components/Insta';
+// import Insta from '../components/Insta';
 
 const Index = () => {
   const posts = usePosts();
